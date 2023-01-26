7c6b5f0942fd0a37ebcbbb5660e93c6b582deb03
Felipe-Freitasleal <felipeleal094@gmail.com>
2023-01-26 20:59:28 -0300

    Primeiro endpoint com requisição.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import axios from 'axios';
-import { get } from 'https';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import axios from "axios";
 
 const app = express();
 
@@ -10,36 +9,35 @@ app.use(express.json());
 app.use(cors());
 
 app.listen(8080, () => {
-    console.log('Servidor na porta 8080');
+  console.log("Servidor na porta 8080");
 });
 
-app.get('/ping', (req: Request, res: Response) => {
-    res.send('pong!')
+app.get("/ping", (req: Request, res: Response) => {
+  res.send("pong!");
 });
 
-app.get('/cors', (req: Request, res: Response) => {
-    res.set('Access-Control-Allow-Origin', '*')
-    res.status(200).send('O CORS funcionou!')
-})
+app.get("/cors", (req: Request, res: Response) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.status(200).send("O CORS funcionou!");
+});
 
-let array: any[] = []
-
-const getArtist = async () => {
-    let a = await axios.get(`https://api.deezer.com/artist/1`)
-}
-
-console.log("console da função", getArtist())
-
-app.get('/artists', (req: Request, res: Response) => {
-    try{
-        res.set('Access-Control-Allow-Origin', '*')
-        res.status(200).send('Porta funcionando')
-    } catch(error: any){
-        console.log(error.message)
-        if (res.statusCode === 200){
-            res.status(500)
-        }
-        res.send(error.message)
+app.get("/artists", async (req: Request, res: Response) => {
+  try {
+    let getArtist: any = await axios.get(`https://api.deezer.com/artist/1`);
+    console.log(getArtist.data);
+    const artists = getArtist.data
+    if (artists) {
+      res.status(200).send(artists);
     }
-
-})
+  } catch (error) {
+    console.log(error.message);
+    if (res.statusCode === 200) {
+      res.status(500);
+    }
+    if (error instanceof Error) {
+      res.send(error.message);
+    } else {
+      res.send("Erro inesperado");
+    }
+  }
+});
